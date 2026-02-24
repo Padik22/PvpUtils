9857e1d2ea20e93da9be94f96707b5f117eb6cc4
Padik22 <dawidek.pawelek1@gmail.com>
2026-02-24 07:31:40 +0100

    Add files via upload

---
+++ PvPUtils.java
@@ -0,0 +1,10 @@
+package com.pvputils;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PvPUtils implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("PvPUtils loaded!");
+    }
+}
